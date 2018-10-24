a6328424476d6ba7c92f064a398d9cb91cbd36b2
Vaelojia <44320350+Vaelojia@users.noreply.github.com>
2018-10-24 16:46:29 +0200

    nice stuff man

---
+++ Assets/StartGame.cs
@@ -0,0 +1,17 @@
+﻿/* using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+  if(Dropdown = Space)
+       {
+       FindObjectOfType<GameManager>().StartGameSpace();
+}
+   if(Dropdown = Jungle)
+       {
+       FindObjectOfType<GameManager>().StartGameJungle();
+       }
+
+}
+*/
